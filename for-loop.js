2e8e9f9cb653a702c9c49cd6ff978a8687bf2eaf
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-14 18:08:18 +0100

    for loops

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+//define a variable total with value 0
+var total = 0;
+//define a variable limit with value 10
+var limit = 10;
+
+//create for loop that iterates over total until limit is reached
+for(var i = 0; i < 10; i ++) {
+  total += i;
+}
+
+//print total to console
+console.log(total);
