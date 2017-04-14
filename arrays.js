c4e5011d3ff4bcf19e4d651152e892da68fe5f03
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-14 19:33:35 +0100

    arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//declare variable pizzaToppings
+var pizzaToppings;
+
+//reference an array with 3 toppings as strings
+pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+
+//print the array in the console
+console.log(pizzaToppings);
