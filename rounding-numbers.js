89d8036b6f02552dec6fee41debb2b551c1e20c4
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-14 17:24:52 +0100

    num to string

---
+++ rounding-numbers.js
@@ -0,0 +1,6 @@
+var roundUp;
+var roundUp = 1.5;
+var rounded;
+rounded = Math.round(roundUp, 0.5);
+
+console.log(rounded);
---
+++ variables.js
@@ -0,0 +1,8 @@
+//declare variable example
+var example;
+
+//make equal to some string
+var example = "some string";
+
+//use console log to print example variable to the console
+console.log(example);
