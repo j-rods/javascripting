820011e250e46944a1464cd91cc8455d2149d1ea
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-14 17:29:37 +0100

    if statements

---
+++ if-statements.js
@@ -0,0 +1,12 @@
+//declare variable fruit
+var fruit;
+//make fruit variable reference value orange(string)
+var fruit = "orange";
+//print "The fruit name has more than five characters."
+//if the length of value fruit has more than 5 characters.
+//otherwise print "The fruit name has five characters or less."
+if(fruit.length > 5) {
+  console.log("The fruit name has more than five characters.")
+} else {
+  console.log("The fruit name has five characters or less.");
+}
