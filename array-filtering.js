0cef09f4bdc7c3456031cc3b6daa1a2dfec3773d
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-14 20:17:22 +0100

    filtering arrays

---
+++ array-filtering.js
@@ -0,0 +1,17 @@
+//declare var numbers
+var numbers;
+
+//make numbers reference array of integers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//declare var filtered that references the result of filtering numbers
+var filtered;
+
+//define a function to filter numbers from the array
+//that are divisible by 2
+filtered = numbers.filter(function (number) {
+    return (number % 2) === 0;
+});
+
+//print filtered array
+console.log(filtered);
