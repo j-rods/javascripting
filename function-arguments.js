b6f3210098a17f79f07f5e480aa1bd1100793a1a
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-22 10:58:57 +0100

    function arguments

---
+++ function-arguments.js
@@ -0,0 +1,11 @@
+// define a function math that takes 3 arguments
+// within math function, return the value obtained
+// from multiplying the second and third arguments
+// and adding the result to the first argument
+function math(num1, num2, num3) {
+  return num1 + (num2 * num3);
+}
+
+//call math() function with the number 53 as first argument,
+// number 61 as second argument, 67 as third argument
+console.log(math(53, 61, 67));
