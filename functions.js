42a6e6e3a50767a8f2e722a1234ac119578dcc65
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-22 10:51:38 +0100

    functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+// define a function eat that takes an argument food
+// food is expected to be a string
+// inside the function, return the food argument
+function eat(food) {
+  return food + " tasted really good.";
+}
+
+//call the eat function with string bananas as argument and print it.
+console.log(eat("bananas"));
